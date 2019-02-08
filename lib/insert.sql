81843a4437cea993e33d669921ce69f7267603df
Cliff Jurgensen <jivefox.002@gmail.com>
2019-02-08 07:37:48 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 16, "male", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "female", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 23, "female", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "female", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 21, "male", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 20, "female", "brown", "instinctive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 101, "male", "brown", "instinctive", 0);
